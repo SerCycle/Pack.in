090c42b3a0010ef76fea54c9b4f10d1a00d162ab
SerCycle <michelyanuar@gmail.com>
2022-04-11 14:21:56 +0700

    Landing finish

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,25 +4,38 @@
     <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        
     <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins" rel="stylesheet">
 
         <title>Pack.in</title>
     </head>
     <body>
         <nav class="mb-auto fixed-top">
-            <div class="bg-warning p-2 text-dark bg-opacity-25">
+            <div class="bg-light p-2 text-dark bg-opacity-50">
                 <div class="d-flex container align-items-center">
-                    <h1 class="pt-1 pb-1">Pack.in</h1>
-                    <span class="pb-1 m-2" style="font-size: 30px">|</span>
+                    <h1 class="pt-1 pb-1" style="font-size: 50px">Pack.in</h1>
+                    <span class="pb-1 ms-3 me-3" style="font-size: 60px">|</span>
                     <p class="pt-3" style="font-size: 30px">About Us</p>
                 </div>
             </div>
         </nav>
-        <div class="bg-image" style="background-image: url('image/landing.jpg'); height: 100vh; background-repeat: no-repeat; background-attachment: fixed; background-size: cover;">
-            <div class="container d-flex justify-content-around">
-                
+        <div class="bg-image p-xl-5 justify-content-center align-items-center" style="font-family: Poppins; background-image: url('image/landing.jpg'); height: 100vh; background-repeat: no-repeat; background-attachment: fixed; background-size: cover;">
+            <div class="container justify-content-center align-items-center">
+                <div class="col-6 pt-5" style="margin-top: 5%; margin-bottom: 10%">
+                    <div class="ps-5 pe-5 bg-dark bg-opacity-25 rounded-3" style="padding-top: 25%; padding-bottom: 25%">
+                        <h1 class="text-white" style="font-size: 50px"><b>Custom Your Perfect Package</b></h1>
+                        <p class="text-white" style="font-size: 25px">Lengkapi keindahan bingkisan & hadiahmu dengan Custome Package</p>
+                        <div class="p-1 d-flex justify-content-center align-items-center">
+                            <div class="m-3 rounded-3">
+                                <a href="home" class="text-white btn p-2" style="background-image: radial-gradient(255.8% 457.4% at 88.45% 24.64%, #BB6552 0%, #646464 100%); font-size: 40px;">
+                                    Start Customize
+                                </a>
+                            </div>
+                        </div> 
+                    </div>
+                </div>
             </div>
         </div>
         
