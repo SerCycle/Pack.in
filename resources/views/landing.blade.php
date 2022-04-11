5bbe5090ab9325b4479c354cf73ed736d6aac86d
SerCycle <michelyanuar@gmail.com>
2022-04-11 13:00:31 +0700

    Add Partial Resources

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+    <head>
+    <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+        <title>Pack.in</title>
+    </head>
+    <body>
+        <nav class="mb-auto fixed-top">
+            <div class="bg-warning p-2 text-dark bg-opacity-25">
+                <div class="d-flex container align-items-center">
+                    <h1 class="pt-1 pb-1">Pack.in</h1>
+                    <span class="pb-1 m-2" style="font-size: 30px">|</span>
+                    <p class="pt-3" style="font-size: 30px">About Us</p>
+                </div>
+            </div>
+        </nav>
+        <div class="bg-image" style="background-image: url('image/landing.jpg'); height: 100vh; background-repeat: no-repeat; background-attachment: fixed; background-size: cover;">
+            <div class="container d-flex justify-content-around">
+                
+            </div>
+        </div>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+</html>
