45840d9fc7192729f62f4adc94293af9b6e2623c
bagustriatm <63024032+bagustriatm@users.noreply.github.com>
2022-04-12 19:16:33 +0700

    Update servicepage.blade.php
    nambahin grid pada item produk

--- resources/views/servicepage.blade.php
+++ resources/views/servicepage.blade.php
@@ -2,8 +2,43 @@
 
 @section('container')
 
-<div class="container card p-2">
-	<h2>Jenis Produk</h2>
+<div class="container card p-2" style="margin-top: 2%; background-color: #BB6552">
+	<h3 style="color: white; padding-left: 2%">Jenis Produk</h3>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm">
+			<div class="card" style="width: 18rem;">
+			  <img class="card-img-top" src="..." alt="Card image cap">
+			  <div class="card-body">
+				<h5 class="card-title">Card title</h5>
+				 
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" class="btn btn-primary">Go somewhere</a>
+			  </div>
+			</div>
+			</div>
+			<div class="col-sm">
+			<div class="card" style="width: 18rem;">
+			  <img class="card-img-top" src="..." alt="Card image cap">
+			  <div class="card-body">
+				<h5 class="card-title">Card title</h5>
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" class="btn btn-primary">Go somewhere</a>
+			  </div>
+			</div>
+			</div>
+			<div class="col-sm">
+			<div class="card" style="width: 18rem;">
+			  <img class="card-img-top" src="..." alt="Card image cap">
+			  <div class="card-body">
+				<h5 class="card-title">Card title</h5>
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" class="btn btn-primary">Go somewhere</a>
+			  </div>
+			</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 @endsection
