a9d4c318dfbca77cf2f1f6941a801bf27c666cc5
SerCycle <michelyanuar@gmail.com>
2022-04-12 17:47:27 +0700

    add footer

---
+++ resources/views/part/footer.blade.php
@@ -0,0 +1,17 @@
+<div class="container-fluid d-flex flex-column h-100" style="background-color:#BB6552;"">
+    <footer class="w-100 py-4 flex-shrink-0">
+        <div class="container py-4">
+            <div class="row text-white">
+                <div class="col-4 text-center">
+                    <h1>Pack.in</h1>
+                </div>
+                <div class="col-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores rem, omnis eos assumenda ducimus alias? Esse perspiciatis reiciendis aperiam, fuga, cumque tenetur commodi, ad possimus optio minima rem inventore itaque.
+                </div>
+                <div class="col-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores rem, omnis eos assumenda ducimus alias? Esse perspiciatis reiciendis aperiam, fuga, cumque tenetur commodi, ad possimus optio minima rem inventore itaque.
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
