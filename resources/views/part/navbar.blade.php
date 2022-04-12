16c517a19547674304d61e017483146c65ec8845
SerCycle <michelyanuar@gmail.com>
2022-04-12 17:20:52 +0700

    make container navbar

--- resources/views/part/navbar.blade.php
+++ resources/views/part/navbar.blade.php
@@ -1,19 +1,23 @@
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#BB6552; padding-left: 5%">
-    <a class="navbar-brand" href="#">Pack.in</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse m-auto d-flex justify-content-center" id="navbarNav">
-    <ul class="navbar-nav">
-    <li class="nav-item active">
-        <a class="nav-link" href="#">Home</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#"><strong>Service</strong></a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">About Us</a>
-    </li>
-    </ul>
+<div class="container-fluid" style="background-color:#BB6552; padding-left: 5%">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <a class="navbar-brand" href="#">Pack.in</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse m-auto d-flex justify-content-center" id="navbarNav">
+            <ul class="navbar-nav pe-4">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Service</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+            </li>
+            </ul>
+            </div>
+        </nav>
     </div>
-</nav>
+</div>
