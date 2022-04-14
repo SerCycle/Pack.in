a383ef7d22f8e1faa7d8d645101fc43ea262388f
hanstl <77946714+hanstl@users.noreply.github.com>
2022-04-15 00:56:16 +0700

    update about and landing page

--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -17,42 +17,7 @@
             </div>
     </div>
 
-<<<<<<< HEAD:resources/views/about.blade.php
-<!-- 
-    <div class="col-8 py-3 px-3">
-=======
 
-    <div class="container col-8 py-3 px-3">
->>>>>>> a138c9935f24c0c775dd52ad74c579d6e3c97475:resources/views/page/about.blade.php
-		<h1 class="text-dark pt-4 px-3" style="font-size: 50px">FAQ</h1>
-
-        
-        
-        <button class="btn px-3" type="button" data-bs-toggle="collapse" data-bs-target="#drop1" aria-expanded="false" aria-controls="drop1">
-            <span class="fa-solid fa-caret-down" style="margin-right:2%" ></span>
-            
-        </button>
-        <span class="fs-4 fw-bold">Bagaimana cara melakukan pembayaran di Pack.in?</span>
-        <div style="min-height: 120px;">
-            <div class="collapse" id="drop1">
-                <div class="card card-body" style="width: 900px;">
-                    Dalam sistem pembayaran, Pack.in menerima transfer melalui bank BCA, Mandiri, BNI, maupun E-Wallet. Untuk nomor rekeningnya seperti tercantum sebagai berikut :
-                </div>
-            </div>
-        </div>
-        <button class="btn px-3" type="button" data-bs-toggle="collapse" data-bs-target="#drop2" aria-expanded="false" aria-controls="drop1">
-            <span class="fa-solid fa-caret-down" style="margin-right:2%" ></span>
-            
-        </button>
-        <span class="fs-4 fw-bold">Bagaimana cara menjual kardus di Packin?</span>
-        <div style="min-height: 120px;">
-            <div class="collapse" id="drop2">
-                <div class="card card-body" style="width: 900px;">
-                    Menu “Jual Kardus” terdapat pada menu Services > Jual Kardus. Kemudian ketika kita klik Jual Sekarang, maka kami akan mengarahkan anda untuk melakukan penjualan kardus tersebut mulai dari pengisian biodata, info kardus, hingga tanggal bersedia diambilnya kardus tersebut
-                </div>
-            </div>
-        </div>
-    </div> -->
 
     <div class="col-8 py-3 px-3">
 		<h1 class="text-dark pt-4 px-3" style="font-size: 50px">FAQ</h1>
--- resources/views/page/landing.blade.php
+++ resources/views/page/landing.blade.php
@@ -40,7 +40,6 @@
         </div>
         
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<<<<<<< HEAD:resources/views/landing.blade.php
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
 	crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
@@ -48,6 +47,4 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
 	crossorigin="anonymous"></script>
     </body>
-=======
->>>>>>> a138c9935f24c0c775dd52ad74c579d6e3c97475:resources/views/page/landing.blade.php
 </html>
