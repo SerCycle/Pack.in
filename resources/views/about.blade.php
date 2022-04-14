ed814a41127609c63fda9602fd5fc55725c0da45
hanstl <77946714+hanstl@users.noreply.github.com>
2022-04-15 00:46:40 +0700

    update page about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -14,14 +14,14 @@
                 <div class="bg-dark bg-opacity-50 py-5" style="height: 50vh; ">
                     <div class="ps-5 pe-5" style="height:50vh">
                     <h1 class="text-white px-1 " style="font-size: 50px;"><b>About Us</b></h1>
-                            <p class="text-white px-1 text-wrap" style="font-size: 25px;">Pack.in merupakan suatu layanan packaging di Indonesia dengan menmanfaatkan box atau kardus bekas yang masih layak untuk digunakan kembali.</p>
+                            <p class="text-white px-1 text-wrap" style="font-size: 25px;">Pack.in merupakan suatu layanan packaging di Indonesia dengan memanfaatkan box atau kardus bekas yang masih layak untuk digunakan kembali.</p>
                     </div>
                     </div>
                 </div>
             </div>
     </div>
 
-
+<!-- 
     <div class="col-8 py-3 px-3">
 		<h1 class="text-dark pt-4 px-3" style="font-size: 50px">FAQ</h1>
 
@@ -51,7 +51,21 @@
                 </div>
             </div>
         </div>
+    </div> -->
+
+    <div class="col-8 py-3 px-3">
+		<h1 class="text-dark pt-4 px-3" style="font-size: 50px">FAQ</h1>
+        <h3 class="text-dark pt-4 px-3" ><i class="fa-solid fa-caret-down" style="margin-right:2%" ></i>Bagaimana cara melakukan pembayaran di Pack.in?</h3>
+        <p class="pt-4 px-3"style="margin-left:4%; margin-right:25%;font-size: 20px">Dalam sistem pembayaran, Pack.in menerima transfer melalui bank BCA, Mandiri, BNI, maupun E-Wallet. Untuk nomor rekeningnya seperti tercantum sebagai berikut :</p>
+        
+        <h3 class="text-dark pt-4 px-3" ><i class="fa-solid fa-caret-down" style="margin-right:2%" ></i>Bagaimana cara menjual kardus di Packin?</h3>
+        <p class="pt-4 px-3"style="margin-left:4%; margin-right:25%;font-size: 20px">Menu “Jual Kardus” terdapat pada menu Services > Jual Kardus. Kemudian ketika kita klik Jual Sekarang, maka kami akan mengarahkan anda untuk melakukan penjualan kardus tersebut mulai dari pengisian biodata, info kardus, hingga tanggal bersedia diambilnya kardus tersebut</p>
+
     </div>
+
+
+
+    
 
 
     <script src="https://kit.fontawesome.com/724f3c3aaf.js" crossorigin="anonymous"></script>
