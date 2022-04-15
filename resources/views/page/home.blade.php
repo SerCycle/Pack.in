e8837a580a2dcd5188635dc04d174890e7545c79
sanizzul <mi.izzulhaq@gmail.com>
2022-04-15 14:24:48 +0700

    Writing Slice Content

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -6,12 +6,12 @@
         <div class="carousel-inner text-center text-white" style="height: 300px">
             <div class="carousel-item active bg-image justify-content-center align-items-center" style="background-image: url('image/about.png'); height: 50vh; background-repeat: no-repeat; background-attachment: fixed; background-size: cover;">
                 <h1 class="p-5" style="font-size: 70px">Custome Package</h1>
-                <p class="ps-5 pe-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis inventore dolore nobis cupiditate molestias iste aliquid doloribus quod quisquam. Neque architecto incidunt tenetur nisi vel nostrum illo sed aperiam magnam!</p>
+                <p class="ps-5 pe-5">Pack.in menyediakan berbagai layanan kustomisasi packaging</p>
             </div>
             <div class="carousel-item bg-image justify-content-center align-items-center" style="background-image: url('image/image5.png'); height: 50vh; background-repeat: no-repeat; background-attachment: fixed; background-size: cover;">
                 <div class="bg-dark bg-opacity-25" style="height: 100%">
                     <h1 class="p-5" style="font-size: 70px">Jual Kardus</h1>
-                    <p class="ps-5 pe-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius deserunt distinctio ullam quod maxime in aliquam tenetur veritatis sapiente nisi sunt ad nesciunt qui, quibusdam adipisci totam vel perferendis quia.</p>
+                    <p class="ps-5 pe-5">Jual kardusmu di Pack.in hanya dengan sekali klik</p>
                 </div>
             </div>
         </div>
