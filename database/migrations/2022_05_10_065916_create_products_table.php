3ee4ecf26bff5b8a41c8ef6bab5b65d8af4e1dc5
DESKTOP-MU55N2L\Alif <michelyanuar@gmail.com>
2022-05-10 18:03:05 +0700

    migrate for product table

---
+++ database/migrations/2022_05_10_065916_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('product_id')->unique()->primary();
+            $table->string('product_name');
+            $table->integer('harga');
+            $table->string('description')->nullable();
+            $table->integer('stock');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
