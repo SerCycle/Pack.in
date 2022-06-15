e1d71eb992bdbc41682e72667796dd9657609597
bagustriatm <bagustri734@gmail.com>
2022-06-15 17:14:34 +0700

    new page. #10

--- resources/views/admin/order_info.blade.php
+++ resources/views/admin/order_info.blade.php
@@ -1,5 +1,53 @@
 @extends('main')
 
 @section('container')
+<div class="container">
+    <h2 style="padding-top: 5%">Product Order Information</h2>
+    <table class="table">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">Order ID</th>
+            <th scope="col">Customer</th>
+            <th scope="col">Deliver Date</th>
+            <th scope="col">Status</th>
+            <th scope="col"></th>
+        </tr>
+        </thead>
+        <tbody>
 
+{{--        item1--}}
+        <tr>
+            <th scope="row">012030055950119</th>
+            <td>Ucup Gunawan</td>
+            <td>01 - 11 - 2022</td>
+            <td>
+                <select class="form-control" id="themecolor" name="theme" style="border: 0px; outline: 0px; max-width: 50%">
+                    <option value="" disabled selected hidden>- status -</option>
+                    <option value="blue">Order Received</option>
+                    <option value="red">On Packing</option>
+                    <option value="green">Delivered</option>
+                </select>
+            </td>
+            <td><u>detail</u></td>
+        </tr>
+
+{{--        item 2--}}
+        <tr>
+            <th scope="row">012030055950118</th>
+            <td>Alif Triatmaja</td>
+            <td>29 - 10 - 2022</td>
+            <td>
+                <select class="form-control" id="themecolor" name="theme" style="border: 0px; outline: 0px; max-width: 50%">
+                    <option value="" disabled selected hidden> - status - </option>
+                    <option value="blue">Order Received </option>
+                    <option value="red">On Packing</option>
+                    <option value="green">Delivered</option>
+                </select>
+            </td>
+            <td><u>detail</u></td>
+        </tr>
+        </tbody>
+    </table>
+
+</div>
 @endsection
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,13 +7,14 @@
 
     <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-		
-		<link href="../css/style.css" rel="stylesheet" type="text/css">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style2.css">
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+		<link href="../css/style.css" rel="stylesheet" type="text/css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="style2.css">
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script type="text/javascript" src="{{ URL::asset('js/bootstrap.bundle.min.js') }}"></script>
+        <script type="text/javascript" src="{{ URL::asset('js/bootstrap.bundle.min.js.map') }}"></script>
 	<script>
 		$(document).ready(function(){
 			$(".default_option").click(function(){
@@ -31,9 +32,9 @@
         <title>{{ $title }}</title>
     </head>
     <body>
-			
+
 		@include('part.navbar')
-		
+
         <div class="container"></div>
         <main>
 			@yield('container')
